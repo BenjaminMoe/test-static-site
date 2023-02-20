4ee3164dc414562ab205b9418fd838425bd8a626
Benjamin Collins <benjamin@collins.moe>
2023-02-21 01:15:44 +0900

    test copilot

---
+++ docs/test.js
@@ -0,0 +1,34 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Function to subtract two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// Function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Function to divide two numbers
+function divide(a, b) {
+    return a / b;
+}
+
+// FFT function
+function fft(a, b) {
+    return a * b;
+}
+
+// Export the functions
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    fft
+};
+
